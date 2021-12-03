d1c4a9951f795038f18d36115a61915fb33acd54
TheDen <deniskhoshaba@gmail.com>
2021-12-04 01:18:39 +1100

    new site

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+hugo --gc --minify -b "https://theden.github.io/thoughts.theden.sh/"
+
+echo "Minifying everything we can"
+find ./public/ -type f \( \
+  -name "*.html" \
+  -o -name '*.js' \
+  -o -name '*.css' \
+  -o -name '*.svg' \
+  -o -name "*.xml" \
+  -o -name "*.json" \
+  -o -name "*.htm" \
+  \) \
+  -and ! -name "*.min*" -print0 |
+  xargs -0 -n1 -P4 -I '{}' sh -c 'minify -o "{}" "{}"'
