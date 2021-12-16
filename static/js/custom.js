0226c42c7239bfca97fd34da5999ee719991cf8f
TheDen <deniskhoshaba@gmail.com>
2021-12-16 13:43:47 +1100

    add music

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,14 @@
+
 function hype() {
 	if (document.getElementById('hype').disabled === false) {
 		document.getElementById('hype').disabled = true
+		music.pause();
 	}
 	else {
 		document.getElementById('hype').disabled = false
+		if (typeof music === 'undefined') {
+			music = new Audio("music/gosh.mp3");
+		}
+		music.play();
 	}
 }
