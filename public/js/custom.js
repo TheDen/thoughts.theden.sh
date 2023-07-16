defc0e2a83112cae4c0d5caccc27b023aeda7f44
TheDen <deniskhoshaba@gmail.com>
2023-07-16 20:58:31 +1000

    remove public folder

--- public/js/custom.js
+++
@@ -1,1 +0,0 @@
-function hype(){document.getElementById("hype").disabled===!1?(document.getElementById("hype").disabled=!0,music.pause()):(document.getElementById("hype").disabled=!1,typeof music=="undefined"&&(music=new Audio("/music/gosh.mp3")),music.play())}
