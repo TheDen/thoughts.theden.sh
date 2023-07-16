2deaff9299d59fe32b85da126a220813a79ededa
TheDen <deniskhoshaba@gmail.com>
2023-07-16 20:49:12 +1000

    add webp

--- create-webp.sh
+++ create-webp.sh
@@ -11,7 +11,7 @@ if ! command -v "${webp_cli}" &> /dev/null; then
   exit
 fi
 
-webp_quality=75
+webp_quality=100
 
 while IFS= read -r -d '' file; do
   cwebp -q "${webp_quality}" "$file" -o "${file%.*}.webp" || true
